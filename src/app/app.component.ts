38725c29006edecd3beae2804557d42c5b5292fc
Andrii <andrewkrinskiy@gmail.com>
2020-02-26 16:01:27 +0200

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+export interface Todo {
+  id: number
+  title: string
+  completed: boolean
+  date?: any
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'test-app';
+
+  public todos = [
+    {}
+  ]
+}
